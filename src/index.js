9170f6c260e9a5c39833cd9f54f8f74dda29b9bd
andela-eisaac <deejecks@live.com>
2016-07-09 12:27:41 +0100

    Course List Page all wired up :)

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { Router, browserHistory } from 'react-router';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
 
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers/index';
 import reduxImmutableStateVariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateVariant())
+    applyMiddleware(thunk, reduxImmutableStateVariant())
   );
 }
